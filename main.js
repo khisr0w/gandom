a1c40063b14b6880996e90032f8f735cdc29f6d9
Khisrow Hashimi <khisrow_hashimi@hotmail.com>
2020-03-22 08:21:11 +0430

    initial commmit

---
+++ main.js
@@ -0,0 +1,33 @@
+// Getting the libararies imported
+
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {
+    app,
+    BrowserWindow,
+    globalShortcut,
+    Menu,
+    ipcMain
+} = electron;
+
+// Block-Scope varaibles
+let mainWindow;
+
+
+
+app.on('ready', () => {
+    openMainWindow();
+});
+
+function openMainWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1920,
+        height: 2018,
+        fullscreen: true,
+        resizable: false,
+    })
+    mainWindow.loadFile("index.html");
+    if (process.env.NODE_ENV == 'production') mainWindow.setMenu(null);
+}
